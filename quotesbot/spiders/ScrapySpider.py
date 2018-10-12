9bfcd5fbbb61dcc12f6b3aa00a2e89e6ab10c497
Daimom <daimom3020@gmail.com>
2018-10-12 08:59:05 +0800

    code

--- quotesbot/spiders/ScrapySpider.py
+++ quotesbot/spiders/ScrapySpider.py
@@ -29,7 +29,7 @@ class booksSpider(scrapy.Spider):
         for learn_node in learn_nodes:
             item = quote()
             item['content'] = "".join(str(learn_node.css('h5 > a::text').extract_first().encode('utf-8')).split())
-            item['source'] = learn_node.css('p.source-book>span.link>a::text').extract_first().encode('utf-8')
+            item['source'] = learn_node.css('p.source-book>span.link>a::text').extract_first()
             item['author'] = str(learn_node.css('p.source-book>span.link:nth-child(2) >a::text').extract_first().encode('utf-8'))
             yield item
             # yield{
